8559f5956aba8b24b4825840b74f69acb5b11120
OksanaSakhno <oksanavenera@bk.ru>
2013-11-14 11:52:32 +0200

    Update BaseTestHelper.java

--- src/test/java/pn/helpers/BaseTestHelper.java
+++ src/test/java/pn/helpers/BaseTestHelper.java
@@ -8,28 +8,30 @@ import pn.configuration.WebDriverFactory;
 
 public class BaseTestHelper {
 	
-	public WebDriver selectBrowser(String nameDriver, String driverPath) {
+	public WebDriver selectBrowser(String nameDriver, PropertiesFileReader propertyReader) {
+		propertyReader.readPropertiesFile(BaseTest.nameFileProperties);
 		log("Select " + nameDriver + " browser");
 		switch (nameDriver) {
 		case "firefox":
 			return WebDriverFactory.getDriver(DesiredCapabilities.firefox());
 		case "chrome": {
-			System.setProperty("webdriver.chrome.driver", driverPath);
+			System.setProperty("webdriver.chrome.driver", propertyReader.getDriverDirChrome());
 			return WebDriverFactory.getDriver(DesiredCapabilities.chrome());
 		}
 		case "IE": {
-			System.setProperty("webdriver.ie.driver", driverPath);
+			System.setProperty("webdriver.ie.driver", propertyReader.getDriverDirIE());
 			return WebDriverFactory.getDriver(DesiredCapabilities
 					.internetExplorer());
 		}
 		case "opera": {
-			System.setProperty("opera.binary", driverPath);
+			System.setProperty("opera.binary", propertyReader.getDriverDirOpera());
 			return WebDriverFactory.getDriver(DesiredCapabilities.opera());
 		}
 		}
 		return WebDriverFactory.getDriver(DesiredCapabilities.firefox());
 	}
-
+	
+	
 	public static void log(String message) {
 		Reporter.log("[LOG]" + " " + message + "<br>");
 	}
