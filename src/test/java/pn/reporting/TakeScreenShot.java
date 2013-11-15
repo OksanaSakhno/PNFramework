ed3579455d55cdc8382557713f630ee437cad79d
OksanaSakhno <oksanavenera@bk.ru>
2013-11-15 10:57:00 +0200

    Update TakeScreenShot.java

--- src/test/java/pn/reporting/TakeScreenShot.java
+++ src/test/java/pn/reporting/TakeScreenShot.java
@@ -8,53 +8,42 @@ import java.util.Date;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
-import static pn.helpers.BaseTestHelper.*;
 import org.testng.TestListenerAdapter;
 import pn.components.Component;
+import static pn.helpers.BaseTestHelper.*;
+
 
 
 public class TakeScreenShot extends TestListenerAdapter {
 
 	public final static String DIRECTORY_FOR_SCREEN= "target/surefire-reports/html/screenshots/";
 	private final static String SCREEN_SHOTS_FILE_EXTENTION = ".png";
-	
+	private static final String HIPERLINK_IMAGE = "<a href=\"%s\"><img src=\"%<s\" width=200 height=150></a><br>";
+
 	
 	public void onTestFailure(ITestResult tr) {
-		super.onTestFailure(tr);
-		System.setProperty("org.uncommons.reportng.escape-output", "false");
-		
-		try {
-			File screenShot = getScreenShot();
-			tr.setAttribute("screenshot", DIRECTORY_FOR_SCREEN + screenShot.getName());
-			tr.setAttribute("screenshotURL", DIRECTORY_FOR_SCREEN + screenShot.getAbsolutePath());
-
-			String logString = String
-					.format("<br /><a href='%s'><img src='%s' hight='100' width='100' /></a>",
-							screenShot.getAbsolutePath(),
-							screenShot.getAbsolutePath());
-
-			log("<br>Screenshot: ");
-			log(logString);
-		} catch (Exception e) {
-			System.out.println("Error generating screenshot: " + e);
-		}
+		takeScreenshot();
 	}
 
-	private File getScreenShot() {
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-		Date date = new Date();
+	public static void takeScreenshot() {
+		WebDriver driver = Component.getDriver();
+		File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		DateFormat dateFormat = new SimpleDateFormat("dd_MM_yyyy__hh_mm_ssaa");
 
-		File screenshot = ((TakesScreenshot) Component.getDriver())
-				.getScreenshotAs(OutputType.FILE);
-		String path = "screenshots" + "/" + dateFormat.format(date) + SCREEN_SHOTS_FILE_EXTENTION;
-		File screenShotInFileSystem = new File(path);
-		
+		String dstFileName = dateFormat.format(new Date()) + SCREEN_SHOTS_FILE_EXTENTION;
+		String relativeFileName = "screenshots/" + dstFileName;
+
+		new File(DIRECTORY_FOR_SCREEN).mkdirs();
+		File destFile = new File(DIRECTORY_FOR_SCREEN + "/" + dstFileName);
+
 		try {
-			FileUtils.copyFile(screenshot, screenShotInFileSystem);
+			FileUtils.copyFile(srcFile, destFile);
 		} catch (IOException e) {
-			System.out.println("Caught IOException");
+			log("IO exception has occured");
 		}
-		return screenShotInFileSystem;
+		log(String.format(HIPERLINK_IMAGE, relativeFileName));
 	}
+
 }
